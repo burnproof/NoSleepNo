0f09095ca7a608de8cf5725c26f7019808a32c41
elvis <elvis@WPC>
2022-08-30 09:57:46 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace NoSleepNo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip3 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.haloToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.Check = new System.Windows.Forms.ToolStripComboBox();
+            this.contextMenuStrip3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip3
+            // 
+            this.contextMenuStrip3.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.haloToolStripMenuItem,
+            this.Check});
+            this.contextMenuStrip3.Name = "contextMenuStrip3";
+            this.contextMenuStrip3.ShowCheckMargin = true;
+            this.contextMenuStrip3.Size = new System.Drawing.Size(204, 53);
+            // 
+            // haloToolStripMenuItem
+            // 
+            this.haloToolStripMenuItem.Name = "haloToolStripMenuItem";
+            this.haloToolStripMenuItem.Size = new System.Drawing.Size(203, 22);
+            this.haloToolStripMenuItem.Text = "Halo";
+            // 
+            // Check
+            // 
+            this.Check.AccessibleDescription = "Check";
+            this.Check.AccessibleName = "Check";
+            this.Check.AccessibleRole = System.Windows.Forms.AccessibleRole.CheckButton;
+            this.Check.Name = "Check";
+            this.Check.Size = new System.Drawing.Size(121, 23);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip3;
+        private System.Windows.Forms.ToolStripMenuItem haloToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox Check;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace NoSleepNo
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MyCustomApplicationContext());
+        }
+    }
+
+
+    public class MyCustomApplicationContext : ApplicationContext
+    {
+        private NotifyIcon TrayIcon;
+        public static bool Enabled = true;
+        private static System.Timers.Timer aTimer;
+        private MenuItem[] MenusS = new MenuItem[2];
+        private int pollRate = Convert.ToInt32(ConfigurationManager.AppSettings["pollRate"]);
+        private static string Button = ConfigurationManager.AppSettings["Button"];
+
+        public MyCustomApplicationContext()
+        {
+            MenusS[0] = new MenuItem("Enabled", Enable);
+            MenusS[0].Checked = Enabled;
+            MenusS[1] = new MenuItem("Exit", Exit);
+            
+            // Initialize Tray Icon
+            TrayIcon = new NotifyIcon()
+            {
+                Icon = Properties.Resources.Icon1,
+                ContextMenu = new ContextMenu(MenusS),
+                Visible = true
+            };
+
+            // Create a timer and set a two second interval.
+            aTimer = new System.Timers.Timer();
+            aTimer.Interval = pollRate;
+            aTimer.Elapsed += OnTimedEvent; // Hook up the Elapsed event for the timer. 
+            aTimer.AutoReset = true; // Have the timer fire repeated events (true is the default)
+            aTimer.Enabled = true; // Start the timer
+        }
+
+        void Enable(object sender, EventArgs e)
+        {
+            Enabled = (!Enabled);
+            MenusS[0].Checked = Enabled;
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            // Hide tray icon, otherwise it will remain shown until user mouses over it
+            TrayIcon.Visible = false;
+
+            Application.Exit();
+        }
+
+        private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            if (Enabled) {
+                SendKeys.SendWait(Button);
+            }   
+        }
+    }
+}
